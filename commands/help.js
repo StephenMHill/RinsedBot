5122369fe97c3c296aa6b69f574cff68a13afffc
Johnny Nguyen <jnguyen1236@gmail.com>
2018-03-10 18:51:03 -0500

    Updated help command

--- commands/help.js
+++ commands/help.js
@@ -6,9 +6,13 @@
  exports.run = (client, message, args) => {
     message.channel.send(`
 Here is a list of the available commands:\n
-**!schedule#** will display that years schedule (replace # with the number).\n
-**!schedules** will display all three schedules.\n
-**!execs** will display the list of club executives.\n
-**!help** will display this list of available commands.
+**!currentnews** - displays the latest news from St. Clair's MAD site
+**!currentevents** - displays the latest events from St. Clair's MAD events
+**!room#** - displays the room schedules. (Replace # with 52, 55 or 61)
+**!rooms** - displays all room schedules
+**!schedule#** - will display that years schedule (replace # with the number).
+**!schedules** - will display all three schedules.
+**!execs** - will display the list of club executives.
+**!help** - will display this list of available commands.
     `);
  }
