d22f25f26717862112dc3536c3dee1441e955d05
Johnny Nguyen <jnguyen1236@gmail.com>
2018-03-11 22:18:32 -0400

    Changed to check if it contains within the string

--- plugins/buzzword.js
+++ plugins/buzzword.js
@@ -13,7 +13,17 @@ var buzzWords = {
 exports.run = (message) => {
     let content = message.content.toLowerCase();
 
-    if (buzzWords[content]) {
-        message.channel.send(buzzWords[content]);
-    }
+    // Transform its content into array
+    let buzzWordsArray = Object.keys(buzzWords);
+
+    // Check if the message contains instead
+    // This needs to be refactored
+    buzzWordsArray.forEach((word) => {
+        // check if it's in
+        let index = content.indexOf(word);
+        if (index !== -1) {
+            message.channel.send(buzzWords[word]);
+            return;
+        }
+    });
 }
