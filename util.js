85a3b0254c7ed52f0530f24d8974937986e77e48
Johnny Nguyen <jnguyen1236@gmail.com>
2018-10-11 21:30:29 -0400

    Updated schedules

--- util.js
+++ util.js
@@ -9,7 +9,7 @@ exports.THIRD_YEAR_SCHEDULE = "./img/Third-Year.png";
 exports.CLASS_SCHEDULES = [exports.FIRST_YEAR_SCHEDULE, exports.SECOND_YEAR_SCHEDULE, exports.THIRD_YEAR_SCHEDULE];
 
 // Room Schedules
-exports.ROOM_52_SCHEDULE = "./img/052_Winter_2018.png";
-exports.ROOM_55_SCHEDULE = "./img/055_Winter_2018.png";
-exports.ROOM_61_SCHEDULE = "./img/061_Winter_2018.png";
+exports.ROOM_52_SCHEDULE = "./img/Room_52_Schedule.jpg";
+exports.ROOM_55_SCHEDULE = "./img/Room_55_Schedule.jpg";
+exports.ROOM_61_SCHEDULE = "./img/Room_61_Schedule.jpg";
 exports.ROOM_SCHEDULES = [exports.ROOM_52_SCHEDULE, exports.ROOM_55_SCHEDULE, exports.ROOM_61_SCHEDULE];
