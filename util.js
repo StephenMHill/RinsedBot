a29b344bc989ae73e708d7a15308fcbac875c364
James DiNovo <james.dinovo01@stclairconnect.ca>
2019-01-07 15:01:38 -0500

    Updated class/room schedules

--- util.js
+++ util.js
@@ -4,12 +4,13 @@
 
 // Class Schedules
 exports.FIRST_YEAR_SCHEDULE = "./img/First-Year.png";
+exports.FIRST_YEAR_SCHEDULE2 = "./img/First-Year2.png";
 exports.SECOND_YEAR_SCHEDULE = "./img/Second-Year.png";
 exports.THIRD_YEAR_SCHEDULE = "./img/Third-Year.png";
-exports.CLASS_SCHEDULES = [exports.FIRST_YEAR_SCHEDULE, exports.SECOND_YEAR_SCHEDULE, exports.THIRD_YEAR_SCHEDULE];
+exports.CLASS_SCHEDULES = [exports.FIRST_YEAR_SCHEDULE, exports.FIRST_YEAR_SCHEDULE2, exports.SECOND_YEAR_SCHEDULE, exports.THIRD_YEAR_SCHEDULE];
 
 // Room Schedules
-exports.ROOM_52_SCHEDULE = "./img/Room_52_Schedule.jpg";
-exports.ROOM_55_SCHEDULE = "./img/Room_55_Schedule.jpg";
-exports.ROOM_61_SCHEDULE = "./img/Room_61_Schedule.jpg";
+exports.ROOM_52_SCHEDULE = "./img/Room_52_Schedule.png";
+exports.ROOM_55_SCHEDULE = "./img/Room_55_Schedule.png";
+exports.ROOM_61_SCHEDULE = "./img/Room_61_Schedule.png";
 exports.ROOM_SCHEDULES = [exports.ROOM_52_SCHEDULE, exports.ROOM_55_SCHEDULE, exports.ROOM_61_SCHEDULE];
