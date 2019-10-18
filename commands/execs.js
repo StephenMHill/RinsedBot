a779d8d22eb411baa10d6499cee293e548dde4e7
StephenMHill <xhillx99@gmail.com>
2019-10-17 22:22:45 -0400

    Update execs.js
    Added Vice President Name

--- commands/execs.js
+++ commands/execs.js
@@ -1,7 +1,7 @@
 exports.run = (client, message, args) => {
     message.channel.send(`
 President: James DiNovo
-Vice-President: TBD
+Vice-President: Stephen Hill
 Social Director: Cordelle Neufeld
 Treasurer: Gregory Gibbons
 Secretary: Curtis Pritchard
